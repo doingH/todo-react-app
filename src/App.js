5cf384be99db3b896e3e04645df6e7bb0abf2c0b
kdhkdh1030@gmail.com <1>
2022-02-15 17:21:44 +0900

    TODO material ui를 사용하여 등록, 삭제

--- src/App.js
+++ src/App.js
@@ -1,29 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
 import { Class } from '@material-ui/icons';
 import React from 'react';
 import Todo from './todo/Todo';
+import { Paper, List, Container } from "@material-ui/core";
+import './App.css';
+import AddTodo from './todo/AddTodo';
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: [
-        {id : 0, title: "Hello World 1", done: true } ,
-        {id : 1, title: "Hello World 2", done: false}
-      ]
+      items: []
     }
   }
-  render() {
 
+  add = (item) => {
+    const thisItems = this.state.items;
+    item.id = "ID-" + thisItems.length; // key를 위한 id추가
+    item.done = false; // done 초기화
+    thisItems.push(item); // 배열에 아이템 추가
+    this.setState({ items: thisItems }); // 업데이트는 반드시 this.setState로 해야됨.
+    console.log("items : ", this.state.items);
+  };
+
+  delete = (item) => {
+    const thisItems = this.state.items;
+    console.log("Before Update Items : ", this.state.items);
+    const newItems = thisItems.filter((e) => e.id !== item.id); // 해당 id 걸러내기
+    this.setState({ items: newItems }, () => {
+      // 디버깅 콜백
+      console.log("Update Items : ", this.state.items);
+    });
+  };
+
+  render() {
     //es6 map함수 이용해 loop
-    var todoItems = this.state.items.map((item, idx) => (
-      <Todo item={item} key={item.id} />
-    ));
+    var todoItems = this.state.items.length > 0 && (
+      <Paper style={{ margine: 16 }}>
+        <List>
+          {this.state.items.map((item, idx) => (
+            <Todo item={item} key={item.id} delete={this.delete} />
+          ))}
+        </List>
+      </Paper>
+    );
 
     return (
       // 컴포넌트 return
-      <div className="App">{todoItems}</div>
+      <div className="App">
+        <Container maxWidth="md">
+          <AddTodo add={this.add} />
+          <div className="TodoList">{todoItems}</div>
+        </Container>
+      </div>
     );
 
   }
--- src/todo/Todo.js
+++ src/todo/Todo.js
@@ -1,25 +1,54 @@
 import React from 'react';
+import {
+    ListItem,
+    ListItemText,
+    InputBase,
+    Checkbox,
+    ListItemSecondaryAction,
+    IconButton,
+  } from "@material-ui/core";
+import { DeleteOutlined } from '@material-ui/icons';
 
 class Todo extends React.Component {
     constructor(props) {
         super(props);
         this.state = { item: props.item };
+        this.delete = props.delete;
     }
+
+    deleteEventHandler = () => {
+        this.delete(this.state.item);
+    };
+
     render() {
         function chkOnchange() {
             alert("1");
         }
 
         return (
-            <div className='Todo'>
-                <input 
-                    type="checkbox" 
-                    id={this.state.item.id}
-                    name={this.state.item.id}
-                    defaultChecked={this.state.item.done}/>
-
-                <label id={this.state.item.id}> {this.state.item.id} </label>
-            </div>
+            <ListItem>
+                <Checkbox checked={this.state.item.done} />
+                <ListItemText>
+                    <InputBase
+                        inputProps={{"aria-label": "naked"}}
+                        type="text"
+                        id={this.state.item.id}
+                        name={this.state.item.id}
+                        value={this.state.item.title}
+                        multiline={true}
+                        fullWidth={true}
+                    >
+                    </InputBase>
+                </ListItemText>
+                <ListItemSecondaryAction>
+                    <IconButton 
+                        aria-label="Delete Todo"
+                        onClick={this.deleteEventHandler}
+                    >
+                        <DeleteOutlined />
+                    </IconButton>
+                </ListItemSecondaryAction>
+            </ListItem>
         );
     }
 }
